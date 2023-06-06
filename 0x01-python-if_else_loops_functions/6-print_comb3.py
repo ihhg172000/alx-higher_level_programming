6fecf67f26a25a8c435d92dd025ab8eb07452752
Islam Hany <ihhg.developer@gmail.com>
2023-06-06 18:16:19 +0300

    add 6-print_comb3.py program

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for first_digit in range(0, 10):
+    end = ", " if first_digit != 8 else "\n"
+    for second_digit in range(first_digit + 1, 10):
+        print("{:d}{:d}".format(first_digit, second_digit), end=end)
