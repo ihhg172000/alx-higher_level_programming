555d8b08d95708eb42cae9bccfe1ad209a5ed6ec
Islam Hany <ihhg.developer@gmail.com>
2023-06-09 00:22:12 +0300

    add 103-fast_alphabet.py program

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from alpha import alpha_upper
+print(alpha_upper)
---
+++ 0x02-python-import_modules/alpha.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+alpha_lower = "abcdefghijklmnopqrstuvwxyz"
+alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
