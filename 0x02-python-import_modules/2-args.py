5a65b0c373377dea0aa466418609256b0e22f0b3
Islam Hany <ihhg.developer@gmail.com>
2023-06-08 22:08:31 +0300

    add 2-args.py program

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def print_argv(argv):
+    for index in range(1, len(argv)):
+        print("{:d}: {:s}".format(index, argv[index]))
+
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(argv) - 1 == 1:
+        print("1 argument:")
+        print_argv(argv)
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        print_argv(argv)
