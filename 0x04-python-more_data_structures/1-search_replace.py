febf2922a97b75711ab4ec976566706845091078
Islam Hany <ihhg.developer@gmail.com>
2023-06-15 21:53:40 +0300

    add only_diff_elements function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda i: replace if i == search else i, my_list))
-    return new_list
+    return list(map(lambda item: replace if item == search else item, my_list))
