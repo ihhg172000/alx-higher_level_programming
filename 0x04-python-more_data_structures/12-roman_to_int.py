1c2740b98ab221dde49f7f2fa3d673f8e9b5785d
Islam Hany <ihhg.developer@gmail.com>
2023-06-16 02:44:17 +0300

    fix roman_to_int function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-from functools import reduce
-
-roman_numerals = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
-
-
-def roman(a, b):
-    if a in roman_numerals:
-        a = roman_numerals[a]
-    if b in roman_numerals:
-        b = roman_numerals[b]
-    if a > b or a == b:
-        return a + b
-    return b - a
-
 
 def roman_to_int(roman_string):
     if type(roman_string) is not str:
         return 0
-    if roman_string in roman_numerals:
-        return roman_numerals[roman_string]
-    return reduce(roman, roman_string)
+
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    numbers = list(map(lambda n: roman_numerals[n], roman_string))
+    result_list = []
+    result = 0
+
+    for i in range(len(numbers)):
+        if numbers[i] <= result:
+            result += numbers[i]
+        else:
+            result = numbers[i] - result
+        if i == len(numbers) - 1 or numbers[i + 1] < numbers[i]:
+            result_list.append(result)
+            result = 0
+
+    return sum(result_list)
