aee6d9bd4b3a463cf1d0973cbbae8ea57a30c0a4
Islam Hany <ihhg.developer@gmail.com>
2023-06-16 17:46:30 +0300

    fix weight_average function

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
     s = sum(list(map(lambda item: item[0] * item[1], my_list)))
     w = sum(list(map(lambda item: item[1], my_list)))
     return s / w
