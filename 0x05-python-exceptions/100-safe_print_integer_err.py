f38d14af9d03b6951f943b55a5da0ebf4a27ac62
Islam Hany <ihhg.developer@gmail.com>
2023-06-27 00:24:11 +0300

    fix safe_print_integer_err function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as msg:
-        print(msg, file=stderr)
+    except (TypeError, ValueError) as msg:
+        print("Exception: {}".format(msg), file=stderr)
         return False
