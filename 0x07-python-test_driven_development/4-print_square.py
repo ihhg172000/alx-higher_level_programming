bb0d62c79218da7b5a9d3f550ed85d47b734e324
Islam Hany <ihhg.developer@gmail.com>
2023-07-06 01:31:56 +0300

    fix 4-print_square.py, 4-print_square.txt files

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,8 +19,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
     for row in range(size):
         for col in range(size):
             print(end="#")
