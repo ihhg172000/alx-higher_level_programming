d3fcb4c10e6864b7d0096c778fd236bc418618d7
Islam Hany <ihhg.developer@gmail.com>
2023-07-12 12:20:55 +0300

    add 10-student.py file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,6 +9,8 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance."""
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
