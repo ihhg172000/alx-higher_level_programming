05cd6226bac1ab62b1c43fae0926906fb46ad013
Islam Hany <ihhg.developer@gmail.com>
2023-07-12 12:11:00 +0300

    add Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""Contains the definition of 'Student' class."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initializes Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+        return self.__dict__
