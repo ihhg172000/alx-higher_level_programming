643c40cc58341aafc62c9ca3db965b7ccda8bd26
Islam Hany <ihhg.developer@gmail.com>
2023-07-16 03:40:29 +0300

    add models/base.py, models/__init__.py files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the definition of 'Base' class.
+"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes Base instance.
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
+        self.id = id
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Contains the unittest for 'Base' class.
+"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """
+    Unit test for 'Base' class.
+    """
+    def test_id(self):
+        """
+        Test id.
+        """
+        b1 = Base()
+        b2 = Base(10)
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 10)
+        self.assertEqual(b3.id, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
