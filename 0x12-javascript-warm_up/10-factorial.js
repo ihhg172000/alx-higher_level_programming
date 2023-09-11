114f68111fc8255d689cecf97c5efc429c559e8b
Islam Hany <ihhg.developer@gmail.com>
2023-09-11 16:11:09 +0300

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+import 'node:process';
+
+const number = process.argv[2];
+
+function factorial (number) {
+  if (number === 0 || isNaN(number)) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(number));
