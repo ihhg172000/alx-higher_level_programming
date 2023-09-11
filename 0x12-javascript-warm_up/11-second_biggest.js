fa909e27c32aab965fcc62d24840956bf9a76ff6
Islam Hany <ihhg.developer@gmail.com>
2023-09-12 01:47:44 +0300

    Fix Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,9 @@ import { argv } from 'node:process';
 if (argv.length === 2 || argv.length === 3) {
   console.log(0);
 } else {
-  const numbers = argv.slice(2);
-
-  numbers.sort((a, b) => {
-    return a === b ? 0 : (a > b ? -1 : 1);
-  });
+  const numbers = argv.slice(2).map(value => {
+    return +value;
+  }).sort((a, b) => b - a);
 
   console.log(numbers[1]);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-lines.forEach((value) => {
-  console.log(value);
-});
