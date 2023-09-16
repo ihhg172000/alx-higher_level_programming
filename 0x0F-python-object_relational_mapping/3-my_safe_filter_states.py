306581a315caf6251f3402b17c95684cdf6e83c2
Islam Hany <ihhg.developer@gmail.com>
2023-09-16 22:26:55 +0300

    Fix SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,9 +8,6 @@ import MySQLdb
 
 if __name__ == '__main__':
 
-    if ';' in argv[4]:
-        quit()
-
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
@@ -22,8 +19,8 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY states.id"
-        .format(argv[4])
+        "SELECT * FROM states WHERE name = BINARY %s ORDER BY states.id",
+        (argv[4], )
     )
 
     query_rows = cursor.fetchall()
