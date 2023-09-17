6cacc915e5ad9fbf732512155d9c77371b990467
Islam Hany <ihhg.developer@gmail.com>
2023-09-17 04:16:55 +0300

    City relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+creates the State “California”
+with the City “San Francisco” from the database hbtn_0e_100_usa.
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = State()
+    state.name = 'California'
+
+    session.add(state)
+    session.commit()
+
+    city = City()
+    city.name = 'San Francisco'
+    city.state_id = state.id
+
+    session.add(city)
+    session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a State and
+an instance Base = declarative_base()
+"""
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from typing import List
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class State(Base):
+    """
+    The definition os State class.
+    """
+    __tablename__ = 'states'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    cities: Mapped[List['City']] = relationship(back_populates='state')
