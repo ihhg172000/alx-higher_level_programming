5cef4fc3e12ebfb7d8b2fd545afbb4f4de321db5
Islam Hany <ihhg.developer@gmail.com>
2023-09-29 15:26:49 +0300

    Fix #2 POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,5 +10,6 @@ from urllib import request, parse
 
 if __name__ == '__main__':
     data = parse.urlencode({'email': argv[2]}).encode('ascii')
-    with request.urlopen(argv[1], data) as response:
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
