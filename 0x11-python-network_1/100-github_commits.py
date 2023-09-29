cd551874ac863c650d3b0c21611eb027949f8cf7
Islam Hany <ihhg.developer@gmail.com>
2023-09-29 21:06:51 +0000

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists 10 commits (from the most recent to oldest)
+"""
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    url = f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits'
+    with requests.get(url) as response:
+        if response.ok:
+            try:
+                commits = reversed(response.json())
+                for i in range(0, 10):
+                    try:
+                        print(
+                            f'{commits[i]["sha"]} '
+                            f'{commits[i]["commit"]["author"]["name"]}'
+                        )
+                    except IndexError:
+                        break
+            except Exception:
+                print('Not a valid JSON')
+        else:
+            print(response.status_code)
