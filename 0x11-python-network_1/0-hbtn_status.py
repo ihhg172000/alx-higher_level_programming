56c8f2a9ed4f74a1adc100ed10507d29a103b3ab
Islam Hany <ihhg.developer@gmail.com>
2023-09-29 14:42:25 +0300

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""  Fetches https://alx-intranet.hbtn.io/status using urllib """
+from urllib import request
+
+
+if __name__ == '__main__':
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print(
+            'Body response:\n'
+            f'\t - type: {type(html)}\n'
+            f'\t - content: {html}\n'
+            f'\t - utf8 content: {html.decode("utf-8")}'
+        )
