c39e1b64f02c1b32ebbed676a073f550a783598f
Islam Hany <ihhg.developer@gmail.com>
2023-09-29 21:32:21 +0000

    Fix #4 time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,18 +9,15 @@ import requests
 if __name__ == '__main__':
     url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
     params = {
-        'per_page' : 10
+        'per_page': 10
     }
     with requests.get(url, params=params) as response:
         if response.ok:
             commits = response.json()
             for commit in commits:
-                try:
-                    print(
-                        f'{commit["sha"]} '
-                        f'{commit["commit"]["author"]["name"]}'
-                    )
-                except IndexError:
-                    break
+                print(
+                    f'{commit["sha"]}: '
+                    f'{commit["commit"]["author"]["name"]}'
+                )
         else:
             print(response.status_code)
