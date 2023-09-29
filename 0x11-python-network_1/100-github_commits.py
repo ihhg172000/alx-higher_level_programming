8e92379a4199a2dff34484852178379535dad245
Islam Hany <ihhg.developer@gmail.com>
2023-09-29 21:15:47 +0000

    Fix #2 time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,17 +10,14 @@ if __name__ == '__main__':
     url = f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits'
     with requests.get(url) as response:
         if response.ok:
-            try:
-                commits = response.json()
-                for i in range(0, 10):
-                    try:
-                        print(
-                            f'{commits[i]["sha"]} '
-                            f'{commits[i]["commit"]["author"]["name"]}'
-                        )
-                    except IndexError:
-                        break
-            except Exception:
-                print('Not a valid JSON')
+            commits = list(reversed(response.json()))
+            for i in range(0, 10):
+                try:
+                    print(
+                        f'{commits[i]["sha"]} '
+                        f'{commits[i]["commit"]["author"]["name"]}'
+                    )
+                except IndexError:
+                    break
         else:
             print(response.status_code)
