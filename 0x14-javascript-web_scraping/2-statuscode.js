0d7dd30931cfc41375f7b819db30b196cdf9a34b
Islam Hany <ihhg.developer@gmail.com>
2023-10-24 15:15:47 +0300

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(
+  process.argv[2],
+  function (error, response) {
+    if (!error) {
+      console.log(`code: ${response.statusCode}`);
+    }
+  }
+);
