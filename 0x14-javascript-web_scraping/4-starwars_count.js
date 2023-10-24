e4ecb616f2eeb9a333161b3a325ab39cf5715b90
Islam Hany <ihhg.developer@gmail.com>
2023-10-24 16:28:29 +0300

    Fix #2 star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,15 +6,17 @@ request(
   function (error, response, body) {
     if (!error && response.statusCode === 200) {
       const films = JSON.parse(body).results;
-      const filtterdFilms = films.filter(
-        function (film) {
-          return film.characters.includes(
-            'https://swapi-api.alx-tools.com/api/people/18/'
-          );
-        }
-      );
+      let count = 0;
 
-      console.log(filtterdFilms.length);
+      films.forEach(film => {
+        film.characters.forEach(character => {
+          if (character.endsWith('/18/')) {
+            count++;
+          }
+        });
+      });
+
+      console.log(count);
     }
   }
 );
