9af3893c42e9219ad683f426962eb13fb1c0ff34
Islam Hany <ihhg.developer@gmail.com>
2023-10-25 10:38:44 +0300

    Fix right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,19 +3,33 @@ const request = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
+function printCharacters (charactersUrls) {
+  let idx = 0;
+
+  function printCharacter (charactersUrl) {
+    request(charactersUrl, (error, response, body) => {
+      if (!error) {
+        const character = JSON.parse(body);
+
+        console.log(character.name);
+
+        try {
+          printCharacter(charactersUrls[++idx]);
+        } catch {
+          // just done
+        }
+      }
+    });
+  }
+
+  printCharacter(charactersUrls[idx]);
+}
+
 request(url, (error, response, body) => {
   if (!error) {
     const film = JSON.parse(body);
     const charactersUrls = film.characters;
 
-    charactersUrls.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (!error) {
-          const character = JSON.parse(body);
-
-          console.log(character.name);
-        }
-      });
-    });
+    printCharacters(charactersUrls);
   }
 });
